6afc811b83f75ca5d98c2727d909114b9b9d4ec7
Shantana Bernados <shantanab@engagespark.com>
2015-05-20 21:38:12 +0800

    initial models

--- smorgastorta/sweden/admin.py
+++ smorgastorta/sweden/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sweden.models import HWQuestion, Assessment, User_HWQ_Assessment
 
 # Register your models here.
+admin.site.register(HWQuestion)
+admin.site.register(Assessment)
+admin.site.register(User_HWQ_Assessment)
--- smorgastorta/sweden/models.py
+++ smorgastorta/sweden/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class HWQuestion(models.Model):
+	srh = models.FloatField()	
+	sleep = models.FloatField()	
+	concentration = models.FloatField()	
+	stress = models.FloatField()
+	energy = models.FloatField()	
+	control = models.FloatField()	
+	social = models.FloatField()	
+	efficiency = models.FloatField()	
+	satisfaction = models.FloatField()	
+	work_load = models.FloatField()	
+	atmosphere = models.FloatField()	
+
+
+class Assessment(models.Model):
+	srh = models.BooleanField()	
+	sleep = models.BooleanField()	
+	sick_leave = models.BooleanField()	
+	
+	
+class User_HWQ_Assessment(models.Model):
+	user = models.ForeignKey(User)
+	hwQ = models.ForeignKey(HWQuestion)
+	ass = models.ForeignKey(Assessment)
+	date = models.DateField(auto_now=True)
+
+
